ed9abc3ee2f90db3b27f9028d620c50413976757
LJG1941 <158084190+LJG1941@users.noreply.github.com>
2024-03-14 15:32:02 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Hello Alert!</h2>
+
+<button onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  alert("Hello< I'm LaChris! Nice to meet you.");
+}
+</script>
+
+</body>
+</html>
